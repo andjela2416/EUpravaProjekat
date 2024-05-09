f0c8e2a002d1a55ee0c8630b7a58bbc2fac7a134
Bojan <babic.bojan62@gmail.com>
2024-05-10 00:00:54 +0200

    Database
    Database connection string and CORS handling

--- euprava/backend/university-service/main.go
+++ euprava/backend/university-service/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
+
+	"backend/university-service/database"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8001"
+	}
+
+	db, err := database.ConnectDB()
+	if err != nil {
+		panic("Failed to connect to the database")
+	}
+	defer db.Close()
+	fmt.Println("Connected to the database")
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Middleware(cors.Config{
+		Origins:         "https://localhost:3000, *",
+		Methods:         "GET, PUT, POST, DELETE, OPTIONS",
+		RequestHeaders:  "Origin, Content-Type,Authorization",
+		ExposedHeaders:  "",
+		MaxAge:          50 * time.Second,
+		Credentials:     true,
+		ValidateHeaders: false,
+	}))
+
+	router.Run(":" + port)
 }
