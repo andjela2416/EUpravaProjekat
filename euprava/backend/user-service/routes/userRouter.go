523064c0dfb94880713ea11037894d35e20ad75f
Bojan <babic.bojan62@gmail.com>
2024-05-14 13:16:09 +0200

    Login and Register

---
+++ euprava/backend/user-service/routes/userRouter.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"backend/controllers"
+	"backend/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(routes *gin.Engine) {
+	routes.Use(middleware.Authentication())
+	routes.GET("/users/:user_id", controllers.GetUser())
+	routes.GET("/users/get", func(c *gin.Context) {
+		controllers.GetUsers(c)
+	})
+}
