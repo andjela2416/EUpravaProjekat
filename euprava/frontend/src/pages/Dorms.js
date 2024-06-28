4106a8cbe71816505577dc893babdae892b1c7f9
Bojan <babic.bojan62@gmail.com>
2024-06-28 02:15:57 +0200

    Front update

---
+++ euprava/frontend/src/pages/Dorms.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dorms = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Dom servis</h1>
+      <br></br>
+      <img src='' alt ='' style={{ width: '100%', height: 'auto' }}></img>
+    </div>
+  );
+};
+
+export default Dorms;
---
+++ euprava/frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Euprava Front</h1>
+      <br></br>
+      <img src='https://i.imgur.com/MQQ7kIn.png' alt ='' style={{ width: '100%', height: 'auto' }}></img>
+    </div>
+  );
+};
+
+export default Home;
