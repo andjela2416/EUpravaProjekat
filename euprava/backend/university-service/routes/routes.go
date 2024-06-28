9bf057b675a1a80d44a8a16561ba7a5c91d77611
Bojan <babic.bojan62@gmail.com>
2024-06-28 02:03:16 +0200

    Front fix

--- euprava/backend/university-service/routes/routes.go
+++ euprava/backend/university-service/routes/routes.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"university-service/controllers"
+	database "university-service/database"
 
 	"github.com/gin-gonic/gin"
 )
 
-func MainRoutes(routes *gin.Engine, uc controllers.UniversityController) {
-	routes.GET("/student/studentID/:student_id", uc.GetStudentByStudentID())
-	routes.POST("/student", uc.InsertStudent())
+func ProfileRoutes(router *gin.Engine) {
+	router.POST("/students/create", controllers.CreateStudentHandler(database.Client))
+	router.GET("/students/:id", controllers.GetStudentByIDHandler(database.Client))
 }
