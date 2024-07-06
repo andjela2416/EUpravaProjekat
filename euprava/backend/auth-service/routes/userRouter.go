e6efec475814b209ff93911b15eec747397c0fb8
Bojan <babic.bojan62@gmail.com>
2024-07-06 12:44:38 +0200

    Registration error fix attempt

--- euprava/backend/auth-service/routes/userRouter.go
+++ euprava/backend/auth-service/routes/userRouter.go
@@ -2,13 +2,12 @@ package routes
 
 import (
 	"backend/controllers"
-	"backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(routes *gin.Engine) {
-	routes.Use(middleware.Authentication())
+	//routes.Use(middleware.Authentication())
 	routes.GET("/users/:user_id", controllers.GetUser())
 	routes.GET("/users/get", func(c *gin.Context) {
 		controllers.GetUsers(c)
