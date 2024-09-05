c76a7f595ec104d420845dbef04b01e5d5f56b2b
Bojan <babic.bojan62@gmail.com>
2024-09-05 02:26:53 +0200

    Complete front Init

--- euprava/frontend/frontend/src/app/app.routes.ts
+++ euprava/frontend/frontend/src/app/app.routes.ts
@@ -6,5 +6,4 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, 
-  { path: '**', redirectTo: '/login' }  
 ];
